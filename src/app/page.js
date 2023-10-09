2564aa3b7f73d00e22daa9f662d8c7e997d54c44
Jevin Jivani <jivani.jevin25@gmail.com>
2023-10-09 23:23:54 +0530

    optical illusion done

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,33 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <p>PSY101 Introduction to Psychology&nbsp;</p>
+      </div>
+
+      <div className={styles.studentInfo}>
+        <div className={styles.info}>
+          <h2>Yagnesh Patel</h2>
+          <h2 className={styles.rollNo}>AU2040025</h2>
+        </div>
+        <div className={styles.info}>
+          <h2>Jevin Jivani</h2>
+          <h2 className={styles.rollNo}>AU2040051</h2>
+        </div>
+        <div className={styles.info}>
+          <h2>Jatin Parmar</h2>
+          <h2 className={styles.rollNo}>AU2040118</h2>
+        </div>
+        <div className={styles.info}>
+          <h2>Yug Patel</h2>
+          <h2 className={styles.rollNo}>AU2040181</h2>
         </div>
       </div>
 
-      <div className={styles.center}>
+      {/* <div className={styles.center}>
         <Image
           className={styles.logo}
           src="/next.svg"
@@ -37,59 +36,63 @@ export default function Home() {
           height={37}
           priority
         />
-      </div>
+      </div> */}
+      <div className={styles.bottomBar}>
+        <div className={styles.title}>
+          <h1 className={styles.bTitle}>Various Illusions</h1>
+          <h4 className={styles.bSubTitle}>Here are some inllusions</h4>
+        </div>
+        <div className={styles.grid}>
+          <a
+            href="/penrose-stairs.html"
+            className={styles.card}
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Penrose Stairs <span>-&gt;</span>
+            </h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          <a
+            href="/black-and-white.html"
+            className={styles.card}
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Black and White <span>-&gt;</span>
+            </h2>
+            <p>
+              Learn about Next.js in an interactive course with&nbsp;quizzes!
+            </p>
+          </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+          <a
+            href="/circles.html"
+            className={styles.card}
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Circle <span>-&gt;</span>
+            </h2>
+            <p>Explore the Next.js 13 playground.</p>
+          </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <a
+            href="/optical.html"
+            className={styles.card}
+            // target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Optical <span>-&gt;</span>
+            </h2>
+            <p>
+              Instantly deploy your Next.js site to a shareable URL with Vercel.
+            </p>
+          </a>
+        </div>
       </div>
     </main>
-  )
+  );
 }
